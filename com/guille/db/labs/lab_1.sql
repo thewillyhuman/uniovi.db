6bf4a7a03c32703ea70d07002a18999006f6126b
Guillermo Facundo Colunga <ZenMaster91@users.noreply.github.com>
2016-01-27 22:42:34 +0100

    Update lab_1.sql

--- com/guille/db/labs/lab_1.sql
+++ com/guille/db/labs/lab_1.sql
@@ -10,6 +10,6 @@ INSERT into coches (codcoche, nombrech, modelo) VALUES ('66','GuillermoCar','4x4
 -- Fourth query.
 DELETE FROM coches WHERE codcoche='66';
 
--- 5th query. (we add the new car in order not to change the natural state of the database)
+-- fifth query. (we add the new car in order not to change the natural state of the database)
 INSERT into coches (codcoche, nombrech, modelo) VALUES ('66','GuillermoCar','4x4');
 UPDATE coches SET nombrech='TheWillyCar' WHERE codcoche='66';
