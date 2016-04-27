e4795f7d225d919634a9255810726cc79def91f6
Guillermo Facundo Colunga <colunga91@gmail.com>
2016-04-27 11:40:30 +0200

    Signed-off-by: Guillermo Facundo Colunga <colunga91@gmail.com>

--- JDBC-WORKINGSET-2016/com/guille/bbdd/jdbc/lab13/Report.java
+++ JDBC-WORKINGSET-2016/com/guille/bbdd/jdbc/lab13/Report.java
@@ -13,19 +13,17 @@ public class Report {
 
     public static void main(String[] args) throws SQLException {
 	try {
-	    DESA.connectDatabase(Database.PROTOCOL_JDBC,
-		    Database.VENDOR_ORACLE, Database.DRIVER_THIN,
-		    Database.DEFAULT_SERVER, Database.DEFAULT_PORT,
-		    Database.DEFAULT_DATABASE, Database.DESA_USER,
+	    DESA.connectDatabase(Database.PROTOCOL_JDBC, Database.VENDOR_ORACLE, Database.DRIVER_THIN,
+		    Database.DEFAULT_SERVER, Database.DEFAULT_PORT, Database.DEFAULT_DATABASE, Database.DESA_USER,
 		    Database.DESA_PASS);
 	} catch (SQLException e) {
 	    e.printStackTrace();
 	}
-	
+
 	listarCochesPorClientes();
-	
+
 	DESA.closeConnection();
-	
+
     }
 
     /**
@@ -36,12 +34,10 @@ public class Report {
     public static void listarCochesPorClientes() throws SQLException {
 	String sql = "SELECT clientes.dni, clientes.nombre, clientes.apellido FROM CLIENTES";
 	String carForClient = "SELECT coches.codcoche, coches.nombrech, coches.modelo, ventas.color "
-		+ "FROM coches, ventas "
-		+ "WHERE ventas.codcoche = coches.codcoche "
-		+ "AND ventas.dni = ?";
+		+ "FROM coches, ventas " + "WHERE ventas.codcoche = coches.codcoche " + "AND ventas.dni = ?";
 	rs = DESA.executeSQL(sql);
-	
-	while(rs.next()) {
+
+	while (rs.next()) {
 	    Integer dni = rs.getInt(1);
 	    String name = rs.getString(2);
 	    String surname = rs.getString(3);
@@ -49,9 +45,10 @@ public class Report {
 	    Object[] parameters = new Object[1];
 	    parameters[0] = dni;
 	    ResultSet subsearch = DESA.executeSQL(carForClient, parameters);
-	    
-	    while(subsearch.next()) {
-		System.out.println("-----> Car: " + subsearch.getInt(1) + " " + subsearch.getString(2) + " " + subsearch.getString(3) + " " + subsearch.getString(4));
+
+	    while (subsearch.next()) {
+		System.out.println("-----> Car: " + subsearch.getInt(1) + " " + subsearch.getString(2) + " "
+			+ subsearch.getString(3) + " " + subsearch.getString(4));
 	    }
 	}
     }
