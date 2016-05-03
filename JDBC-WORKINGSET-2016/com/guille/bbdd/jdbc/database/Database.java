f7daf49923e0a80057e1fd7930364cdd37f92d50
Guillermo Facundo Colunga <colunga91@gmail.com>
2016-05-03 14:19:42 +0200

    Signed-off-by: Guillermo Facundo Colunga <colunga91@gmail.com>

--- JDBC-WORKINGSET-2016/com/guille/bbdd/jdbc/database/Database.java
+++ JDBC-WORKINGSET-2016/com/guille/bbdd/jdbc/database/Database.java
@@ -23,9 +23,97 @@ public abstract class Database {
 
     protected Connection conn = null;
     protected Statement stat = null;
+    protected PreparedStatement psQuery = null;
+    protected CallableStatement cs = null;
     protected ResultSet rs = null;
 
     protected Database() {
+    }
+
+    /**
+     * First out parameters and then in parameters!!!
+     * 
+     * @param statement
+     *            to call the Store Procedure or parameter.
+     * @param types
+     *            of the in parameters.
+     * @param parameters
+     *            for the function/stored procedure.
+     * @return and Object array with all the solutions.
+     * @throws SQLException
+     */
+    public Object[] callFunction(String statement, Integer[] types, Object[] parameters) throws SQLException {
+	this.cs = this.conn.prepareCall(statement);
+	int i = 1;
+	for (Integer s : types) {
+	    this.cs.registerOutParameter(i, s);
+	    i++;
+	}
+
+	for (Object o : parameters) {
+	    this.cs.setObject(i, o);
+	    i++;
+	}
+
+	this.cs.execute();
+
+	Object[] result = new Object[types.length];
+	for (i = 1; i <= result.length; i++) {
+	    Object o = cs.getObject(i);
+	    result[i - 1] = o;
+	}
+
+	return result;
+    }
+
+    /**
+     * First out parameters and then in parameters!!!
+     * 
+     * @param statement
+     *            to call the Store Procedure or parameter.
+     * @param types
+     *            of the in parameters.
+     * @param parameters
+     *            for the function/stored procedure.
+     * @return and Object array with all the solutions.
+     * @throws SQLException
+     */
+    public Object[] callStoredProcedure(String statement, Integer[] types, Object[] parameters) throws SQLException {
+	this.cs = this.conn.prepareCall(statement);
+	int i = 1;
+	for (Integer s : types) {
+	    this.cs.registerOutParameter(i, s);
+	    i++;
+	}
+	i = 1;
+	for (Object o : parameters) {
+	    this.cs.setObject(i, o);
+	    i++;
+	}
+	this.cs.execute();
+	Object[] result = new Object[types.length];
+	for (i = 1; i <= result.length; i++) {
+	    Object o = this.cs.getObject(i);
+	    result[i - 1] = o;
+	}
+
+	return result;
+    }
+
+    /**
+     * Closes a given connection.
+     * 
+     * @param connection
+     *            to be closed.
+     */
+    public void closeConnection() {
+	try {
+	    this.conn.close();
+	    System.out.println("--> Connection " + this.conn.toString() + " has been closed succesfully.");
+	} catch (SQLException e) {
+	    System.err.println("The connection couldn't be closed.");
+	    e.printStackTrace();
+	}
     }
 
     /**
@@ -56,22 +144,6 @@ public abstract class Database {
 	    String port, String databaseName, String user, String password) throws SQLException;
 
     /**
-     * Closes a given connection.
-     * 
-     * @param connection
-     *            to be closed.
-     */
-    public void closeConnection() {
-	try {
-	    this.conn.close();
-	    System.out.println("--> Connection " + this.conn.toString() + " has been closed succesfully.");
-	} catch (SQLException e) {
-	    System.err.println("The connection couldn't be closed.");
-	    e.printStackTrace();
-	}
-    }
-
-    /**
      * Executes a sql in a given connection.
      * 
      * @param sql
@@ -100,11 +172,11 @@ public abstract class Database {
      *             if any error.
      */
     public ResultSet executeSQL(String sql, Object[] parameters) throws SQLException {
-	PreparedStatement psQuery = this.conn.prepareStatement(sql);
+	this.psQuery = this.conn.prepareStatement(sql);
 	for (int i = 0; i < parameters.length; i++) {
-	    psQuery.setObject(i + 1, parameters[i]);
+	    this.psQuery.setObject(i + 1, parameters[i]);
 	}
-	this.rs = psQuery.executeQuery();
+	this.rs = this.psQuery.executeQuery();
 	return getResultSet();
     }
 
@@ -120,81 +192,11 @@ public abstract class Database {
      * @throws SQLException
      */
     public void executeUpdate(String sql, Object[] parameters) throws SQLException {
-	PreparedStatement psQuery = this.conn.prepareStatement(sql);
+	this.psQuery = this.conn.prepareStatement(sql);
 	for (int i = 0; i < parameters.length; i++) {
-	    psQuery.setObject(i + 1, parameters[i]);
+	    this.psQuery.setObject(i + 1, parameters[i]);
 	}
-	psQuery.executeUpdate();
-    }
-
-    /**
-     * First out parameters and then in parameters!!!
-     * 
-     * @param statement
-     *            to call the Store Procedure or parameter.
-     * @param types
-     *            of the in parameters.
-     * @param parameters
-     *            for the function/stored procedure.
-     * @return and Object array with all the solutions.
-     * @throws SQLException
-     */
-    public Object[] callStoredProcedure(String statement, Integer[] types, Object[] parameters) throws SQLException {
-	CallableStatement cs = this.conn.prepareCall(statement);
-	int i = 1;
-	for (Integer s : types) {
-	    cs.registerOutParameter(i, s);
-	    i++;
-	}
-	i = 1;
-	for (Object o : parameters) {
-	    cs.setObject(i, o);
-	    i++;
-	}
-	cs.execute();
-	Object[] result = new Object[types.length];
-	for (i = 1; i <= result.length; i++) {
-	    Object o = cs.getObject(i);
-	    result[i - 1] = o;
-	}
-
-	return result;
-    }
-
-    /**
-     * First out parameters and then in parameters!!!
-     * 
-     * @param statement
-     *            to call the Store Procedure or parameter.
-     * @param types
-     *            of the in parameters.
-     * @param parameters
-     *            for the function/stored procedure.
-     * @return and Object array with all the solutions.
-     * @throws SQLException
-     */
-    public Object[] callFunction(String statement, Integer[] types, Object[] parameters) throws SQLException {
-	CallableStatement cs = this.conn.prepareCall(statement);
-	int i = 1;
-	for (Integer s : types) {
-	    cs.registerOutParameter(i, s);
-	    i++;
-	}
-
-	for (Object o : parameters) {
-	    cs.setObject(i, o);
-	    i++;
-	}
-
-	cs.execute();
-
-	Object[] result = new Object[types.length];
-	for (i = 1; i <= result.length; i++) {
-	    Object o = cs.getObject(i);
-	    result[i - 1] = o;
-	}
-
-	return result;
+	this.psQuery.executeUpdate();
     }
 
     /**
@@ -204,6 +206,24 @@ public abstract class Database {
      */
     public ResultSet getResultSet() {
 	return this.rs;
+    }
+
+    /**
+     * Returns the number of columns the ResultSet has.
+     * 
+     * @param rs
+     *            to compute.
+     * @return the number of rows that it contains.
+     * @throws SQLException
+     */
+    public int getResutSetSize() throws SQLException {
+	int aux = 0;
+	while (this.rs != null) {
+	    this.rs.beforeFirst();
+	    this.rs.last();
+	    aux = this.rs.getRow();
+	}
+	return aux;
     }
 
     /**
@@ -230,23 +250,5 @@ public abstract class Database {
 	    }
 	}
 	return toReturn;
-    }
-
-    /**
-     * Returns the number of columns the ResultSet has.
-     * 
-     * @param rs
-     *            to compute.
-     * @return the number of rows that it contains.
-     * @throws SQLException
-     */
-    public int getResutSetSize() throws SQLException {
-	int aux = 0;
-	while (this.rs != null) {
-	    this.rs.beforeFirst();
-	    this.rs.last();
-	    aux = this.rs.getRow();
-	}
-	return aux;
     }
 }
